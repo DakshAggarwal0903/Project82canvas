4a513fd7793aaa535fe6c165a355ad379a360b0c
StormMaster99 <74252852+StormMaster99@users.noreply.github.com>
2021-04-27 15:34:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ mouseEvent = "empty";
 var oldX ,oldY;
 var canvas = document.getElementById("myCanvas");
  var ctx=canvas.getContext("2d");
- var color = "blue";
- var width = 1;
- var radius = 12;
+
 canvas.addEventListener("mousedown",msdn);
 function msdn(g){
     color = document.getElementById("col").value;
@@ -29,9 +27,9 @@ function msde(v){
         ctx.strokeStyle = color;
         ctx.lineWidth = width;
         console.log("Last position of x coordinate: "+oldX+"and the Y coordinate: "+oldY);
-        ctx.moveTo(oldX,oldY);
+       
         console.log("New position of x coordinate: "+x+"and the Y coordinate: "+y);
-        ctx.lineTo(x,y);
+       
         ctx.arc(x,y,radius,0 ,2 * Math.PI);
         ctx.stroke();
     }
